b641896afd7499c7b495d1fe10959929e1e46b3d
Hamzshaa <trident32000@gmail.com>
2024-02-12 05:06:38 -0800

    vite+react and tailwind css initialized

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <div className="flex space-between">akdjf</div>
+      <p className="text-3xl">aksdjf</p>
+    </>
+  );
+}
+
+export default App;
