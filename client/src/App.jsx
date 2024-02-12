e19241b7c1a3d19a7799b1c695dcffff98157e7d
Hamzshaa <trident32000@gmail.com>
2024-02-12 07:08:17 -0800

    header created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,25 @@
-import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <>
-      <div className="flex space-between">akdjf</div>
-      <p className="text-3xl">aksdjf</p>
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
