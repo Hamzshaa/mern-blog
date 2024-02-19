7a40c2a2098ed7dec59bdc2fd62da5af932d3009
Hamzshaa <trident32000@gmail.com>
2024-02-19 13:09:50 -0800

    add comment section to the post page

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import postRoutes from "./routes/post.route.js";
+import commentRouter from "./routes/comment.route.js";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
@@ -25,6 +26,7 @@ app.use(cookieParser());
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
+app.use("/api/comment", commentRouter);
 
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
